9f4967a82db86eb153d1f63cb9b40856f83232bd
stephanie_FAROUX <stephanie_FAROUX@231ead53-1ad4-42d1-bb47-382d422dd664>
2011-11-28 13:50:19 +0000

    Coefficient for the calculation of SSO_DRAG removed
    git-svn-id: http://svn.cnrm-game-meteo.fr/projets/surfex/trunk@5 231ead53-1ad4-42d1-bb47-382d422dd664

--- SURFEX/sso_beljaars04.F90
+++ SURFEX/sso_beljaars04.F90
@@ -84,7 +84,7 @@ C_AVAR    = C_K1**(C_N1-C_N2) / (C_IH * C_KFLT**C_N1)   ! (unit: m^{1+C_N2}  =  
 !
 ZSSO_DRAG = 0.
 DO JL=1,KLVL
-  ZSSO_DRAG (:,JL) = 20. * C_ALPHA * C_BETA * C_COR * C_CMD * 2.109 * EXP( -(PZ(:,JL)/1500.)**1.5) &
+  ZSSO_DRAG (:,JL) = C_ALPHA * C_BETA * C_COR * C_CMD * 2.109 * EXP( -(PZ(:,JL)/1500.)**1.5) &
     * C_AVAR * PSSO_STDEV(:)**2 * PZ(:,JL)**(-1.2)
 END DO
 !
