60f9c48f321c1c9e8be568fe42d679e29627b4e7
stephanie_FAROUX <stephanie_FAROUX@231ead53-1ad4-42d1-bb47-382d422dd664>
2013-02-15 13:53:20 +0000

    Integration of revision 859
    git-svn-id: http://svn.cnrm-game-meteo.fr/projets/surfex/trunk@862 231ead53-1ad4-42d1-bb47-382d422dd664

--- src/OFFLIN/assim_isba_update_snow.F90
+++ src/OFFLIN/assim_isba_update_snow.F90
@@ -11,10 +11,12 @@ SUBROUTINE ASSIM_ISBA_UPDATE_SNOW(YPROGRAM, KI, PSWE, PSWE_ORIG, LINITSNOW, LINC
 #ifdef LFI
  USE MODD_IO_SURF_LFI, ONLY : CFILEIN_LFI,CFILEOUT_LFI
 #endif
+ USE MODN_IO_OFFLINE,  ONLY : CPREPFILE
+ USE YOMHOOK,          ONLY : LHOOK,DR_HOOK
+ USE PARKIND1,         ONLY : JPRB
  USE MODD_CSTS,        ONLY : XTT
  USE MODD_SURF_PAR,    ONLY : XUNDEF
-!
- USE MODN_IO_OFFLINE,  ONLY : CPREPFILE
+ USE MODD_SNOW_PAR,    ONLY : XANSMIN, XANSMAX, XRHOSMIN, XRHOSMAX
 !
  USE MODI_ABOR1_SFX
  USE MODI_INIT_IO_SURF_n 
@@ -23,29 +25,30 @@ SUBROUTINE ASSIM_ISBA_UPDATE_SNOW(YPROGRAM, KI, PSWE, PSWE_ORIG, LINITSNOW, LINC
  USE MODI_IO_BUFF_CLEAN_n
  USE MODI_FLAG_UPDATE
  USE MODI_WRITE_SURF
-!
- USE YOMHOOK,          ONLY : LHOOK,DR_HOOK
- USE PARKIND1,         ONLY : JPRB
+
 !
  IMPLICIT NONE
 !
  CHARACTER(LEN=6),    INTENT(IN)    :: YPROGRAM  ! program calling surf. schemes
  INTEGER,             INTENT(IN)    :: KI
- REAL, DIMENSION(KI),  INTENT(IN)    :: PSWE
- REAL, DIMENSION(KI),  INTENT(INOUT) :: PSWE_ORIG
+ REAL, DIMENSION(KI), INTENT(IN)    :: PSWE
+ REAL, DIMENSION(KI), INTENT(INOUT) :: PSWE_ORIG
  LOGICAL,             INTENT(IN)    :: LINITSNOW
  LOGICAL,             INTENT(IN)    :: LINC
  CHARACTER(LEN=2),    INTENT(IN)    :: HTEST     ! must be equal to 'OK'
 !
 !    Declarations of local variables
 !
- REAL, DIMENSION(KI)       :: ZSWE     ! Snow before update
- REAL, DIMENSION(KI)       :: ZSWEINC
- REAL, DIMENSION(KI)       :: PTS
+ REAL(KIND=JPRB)                   :: ZHOOK_HANDLE
  CHARACTER(LEN=10)                 :: YVAR    ! Name of the prognostic variable (in LFI file)
  CHARACTER(LEN=100)                :: YPREFIX ! Prefix of the prognostic variable  (in LFI file)
  INTEGER                           :: IRESP
- REAL(KIND=JPRB)                   :: ZHOOK_HANDLE
+ REAL, DIMENSION(KI)               :: ZSWE     ! Snow before update
+ REAL, DIMENSION(KI)               :: ZSWEINC
+ REAL, DIMENSION(KI)               :: PTS
+!    Addtional snow fields with D95 snow scheme 
+ REAL, DIMENSION(KI)               :: ZSNR     ! Snow density 
+ REAL, DIMENSION(KI)               :: ZSNA     ! Snow albedo 
 !
 !
 ! ----------------------------------------------------------------------------------
@@ -65,7 +68,7 @@ SUBROUTINE ASSIM_ISBA_UPDATE_SNOW(YPROGRAM, KI, PSWE, PSWE_ORIG, LINITSNOW, LINC
    CFILEIN_LFI = CPREPFILE        ! input PREP file (surface fields)
 #endif
    CALL INIT_IO_SURF_n(YPROGRAM,'NATURE','SURF  ','READ ')
-   CALL READ_SURF(YPROGRAM,'WSNOW_VEG1',PSWE_ORIG,  IRESP)
+   CALL READ_SURF(YPROGRAM,'WSNOW_VEG1',ZSWE,  IRESP)
    CALL READ_SURF(YPROGRAM,'TG1',       PTS,   IRESP)
    CALL END_IO_SURF_n(YPROGRAM)
    CALL IO_BUFF_CLEAN_n
@@ -75,6 +78,11 @@ SUBROUTINE ASSIM_ISBA_UPDATE_SNOW(YPROGRAM, KI, PSWE, PSWE_ORIG, LINITSNOW, LINC
       ZSWE(:)   = 0.0
    END WHERE
 
+   ZSWE=PSWE
+   ! Set snow=0 where 1. guess = 0 and Ts>0, to avoid that the snow analysis introduce snow where it is no snow.
+   WHERE ( PSWE(:)/=XUNDEF .AND. PSWE(:)<1.0E-10 .AND. PTS(:)>XTT )
+      ZSWE(:)   = 0.0
+   END WHERE
  ENDIF
 
  ! Update snow
@@ -84,7 +92,24 @@ SUBROUTINE ASSIM_ISBA_UPDATE_SNOW(YPROGRAM, KI, PSWE, PSWE_ORIG, LINITSNOW, LINC
    ZSWEINC(:) = ZSWE(:) - PSWE_ORIG(:)
    WRITE(*,'("  SURFRESERV.NEIGE - min, mean, max: ",3E13.4)') MINVAL(ZSWE),MAXVAL(ZSWE),SUM(ZSWE)/KI
    WRITE(*,*) 'Mean SN increments over NATURE ',SUM(ZSWEINC)/KI
-
+ 
+   !   read additional snow fields 
+#ifdef LFI 
+   CFILEIN_LFI = CPREPFILE        ! input PREP file (surface fields) 
+#endif 
+   CALL INIT_IO_SURF_n(YPROGRAM,'NATURE','SURF  ','READ ') 
+   CALL READ_SURF(YPROGRAM,'RSNOW_VEG1',ZSNR,  IRESP) 
+   CALL READ_SURF(YPROGRAM,'ASNOW_VEG', ZSNA,  IRESP) 
+   CALL END_IO_SURF_n(YPROGRAM) 
+   CALL IO_BUFF_CLEAN_n 
+ 
+   ! Snow albedo and density are given initial values in points  
+   ! which get initial snow in the snow analysis 
+   WHERE ( PSWE_ORIG(:) < 1.0E-10 .AND. ZSWE(:)>= 1.0E-10 ) 
+   ZSNA(:)    = 0.5 * ( XANSMIN + XANSMAX ) 
+   ZSNR(:)    = 0.5 * ( XRHOSMIN + XRHOSMAX ) 
+   END WHERE 
+   
  ENDIF
 
  ! Write updated snow
@@ -98,7 +123,14 @@ SUBROUTINE ASSIM_ISBA_UPDATE_SNOW(YPROGRAM, KI, PSWE, PSWE_ORIG, LINITSNOW, LINC
  YVAR='WSNOW_VEG1'
  YPREFIX='X_Y_WSNOW_VEG1 (kg/m2)                            '
  CALL WRITE_SURF(YPROGRAM,YVAR,ZSWE,IRESP,HCOMMENT=YPREFIX)
-
+ IF ( LINC ) THEN 
+   YVAR='RSNOW_VEG1' 
+   YPREFIX='X_Y_RSNOW_VEG1 (kg/m3)                            ' 
+   CALL WRITE_SURF(YPROGRAM,YVAR,ZSNR,IRESP,HCOMMENT=YPREFIX) 
+   YVAR='ASNOW_VEG' 
+   YPREFIX='X_Y_ASNOW_VEG1 (%)                            ' 
+   CALL WRITE_SURF(YPROGRAM,YVAR,ZSNA,IRESP,HCOMMENT=YPREFIX) 
+ ENDIF 
  CALL END_IO_SURF_n(YPROGRAM)
  CALL IO_BUFF_CLEAN_n
 
