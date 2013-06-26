ddf7a3d470e0d8f999338f5a2929eb5fdda29364
stephanie_FAROUX <stephanie_FAROUX@231ead53-1ad4-42d1-bb47-382d422dd664>
2013-06-26 14:41:19 +0000

    Back to revision 1034
    git-svn-id: http://svn.cnrm-game-meteo.fr/projets/surfex/trunk@1073 231ead53-1ad4-42d1-bb47-382d422dd664

--- src/SURFEX/flag_teb_gardenn.F90
+++ src/SURFEX/flag_teb_gardenn.F90
@@ -40,12 +40,10 @@
 USE MODD_CO2V_PAR,       ONLY : XANFMINIT, XCONDCTMIN
 USE MODD_TEB_n,          ONLY : XGARDEN
 USE MODD_TEB_VEG_n,      ONLY : CPHOTO, CISBA, CRESPSL
-USE MODD_TEB_GARDEN_OPTION_n,  ONLY : NGROUND_LAYER
-USE MODD_TEB_GARDEN_PGD_n,     ONLY : XBSLAI
-USE MODD_TEB_GARDEN_PGD_EVOL_n,ONLY : XLAI
-USE MODD_TEB_GARDEN_n,   ONLY : XTG, XWG, XWGI, XWR, TSNOW,         &
+USE MODD_TEB_GARDEN_n,   ONLY : NGROUND_LAYER,                      &
+                                XTG, XWG, XWGI, XWR, XLAI, TSNOW,   &
                                 XRESA, XANFM, XAN, XLE, XANDAY,     &
-                                XBIOMASS, XRESP_BIOMASS,            &
+                                XBSLAI, XBIOMASS, XRESP_BIOMASS,    &
                                 XSNOWFREE_ALB, XSNOWFREE_ALB_VEG,   &
                                 XSNOWFREE_ALB_SOIL
 !                                
