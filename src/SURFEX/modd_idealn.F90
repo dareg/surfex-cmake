e297e71bf5512e57ffb7c6afff8f959c92994007
stephanie_FAROUX <stephanie_FAROUX@231ead53-1ad4-42d1-bb47-382d422dd664>
2013-06-26 14:41:34 +0000

    Back to revision 1034
    git-svn-id: http://svn.cnrm-game-meteo.fr/projets/surfex/trunk@1094 231ead53-1ad4-42d1-bb47-382d422dd664

--- src/SURFEX/modd_idealn.F90
+++ src/SURFEX/modd_idealn.F90
@@ -62,8 +62,6 @@ INTEGER, INTENT(IN) :: KFROM, KTO
 REAL(KIND=JPRB) :: ZHOOK_HANDLE
 !
 ! Save current state for allocated arrays
-IF (LKFROM) THEN
-ENDIF
 !
 ! Current model is set to model KTO
 IF (LHOOK) CALL DR_HOOK('MODD_IDEAL_N:IDEAL_GOTO_MODEL',0,ZHOOK_HANDLE)
