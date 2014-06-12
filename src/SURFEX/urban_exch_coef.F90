0220be16220ab973650286bc373a184b722b11a7
stephanie_FAROUX <stephanie_FAROUX@231ead53-1ad4-42d1-bb47-382d422dd664>
2014-06-12 09:21:35 +0000

    To avoir warning messages in DEBUG on BEAUFIX
    git-svn-id: http://svn.cnrm-game-meteo.fr/projets/surfex/branches/V8_VM_BUGS@2104 231ead53-1ad4-42d1-bb47-382d422dd664

--- src/SURFEX/urban_exch_coef.F90
+++ src/SURFEX/urban_exch_coef.F90
@@ -96,7 +96,7 @@ REAL, DIMENSION(SIZE(PZ0))         :: ZZ0H     ! roughness length for heat
 REAL,DIMENSION(SIZE(PTA)) :: cmu, ctu, rib,ftemp,fvap,ilmo  ! temporary var for
 REAL,DIMENSION(SIZE(PTA)) :: ue,fcor, hBL,lzz0,lzz0t,fm, fh ! flxsurf3
 REAL,DIMENSION(SIZE(PTA)) :: z0h_roof,z0h_town,z0h_road     ! local thermal roughness
-REAL,DIMENSION(SIZE(PTA)) :: zustar
+REAL,DIMENSION(SIZE(PTA)) :: zustar, zta, ztg
 REAL,DIMENSION(SIZE(PTA)) :: ZVMOD                          ! wind
 INTEGER N
 !
@@ -143,10 +143,11 @@ ELSEIF(HZ0H=='BRUT82' .OR. HZ0H=='KAND07')THEN
     ZZ0H(:)= PZ0(:) * 7.4 * EXP( - 2.46 *( PZ0(:)*zustar(:)/1.461e-5)**0.25)
   ENDIF
 
-
+  ZTA = PTA/PEXNA
+  ZTG = PTG/PEXNS
   CALL FLXSURF3BX( cmu, ctu, PRI,ftemp,fvap,ilmo,    &
-                   ue, fcor, PTA/PEXNA, PQA,           &
-                   PUREF, PZREF, ZVMOD, PTG/PEXNS, PQS,&
+                   ue, fcor, ZTA, PQA,           &
+                   PUREF, PZREF, ZVMOD, PTG, PQS,&
                    hBL, PZ0,ZZ0H,                      &
                    lzz0, lzz0t, fm, fh,N               )  
 !
