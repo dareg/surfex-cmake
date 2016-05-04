162a3e30ec177b2e989513bc9808e270beece08d
stephanie_FAROUX <stephanie_FAROUX@231ead53-1ad4-42d1-bb47-382d422dd664>
2016-05-04 14:58:35 +0000

    Correction
    git-svn-id: http://svn.cnrm-game-meteo.fr/projets/surfex/branches/trunk_parall@3748 231ead53-1ad4-42d1-bb47-382d422dd664

--- src/SURFEX/read_direct_gauss.F90
+++ src/SURFEX/read_direct_gauss.F90
@@ -369,7 +369,11 @@ DO
       ZVALUE(:)=YVALUE16(:)
       !
       IF (ICPT==0) THEN      
-        IF (      ANY(ABS(ZVALUE)>15000)   ) THEN
+        IF ( (HFIELD(1:5)=="COVER" .AND. (ANY(ZVALUE>573.) .OR. ANY(ZVALUE<0.)) ) .OR. & 
+            ((HFIELD(1:4)=="SAND" .OR. HFIELD(1:4)=="CLAY") .AND. (ANY(ZVALUE>100.) .OR. ANY(ZVALUE<0.)) ) .OR. &
+             (HFIELD(1:3)=="SOC" .AND. (ANY(ZVALUE>15000.) .OR. ANY(ZVALUE<0.)) )  .OR. &
+             ((HFIELD(1:5)/="COVER" .AND. HFIELD(1:4)/="SAND" .AND. HFIELD(1:4)/="CLAY" .AND. &
+              HFIELD(1:3)/="SOC" .AND. ANY(ZVALUE>15000.) ) ) ) THEN
           GFLAG = .FALSE.
           ICPT = ICPT + 1      
           IF (GSWAP) CALL ABOR1_SFX('READ_DIRECT_GAUSS: SWAP ALREADY DONE, CANNOT BE REDONE')
