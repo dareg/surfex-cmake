0d4f0b75f59686787d67da28a73526e744d34cad
Matthieu Lafaysse <matthieu.lafaysse@meteo.fr>
2018-01-12 13:55:34 +0100

    write library paths for vortex

--- src/Makefile.SURFEX.mk
+++ src/Makefile.SURFEX.mk
@@ -17,6 +17,14 @@ ARCH_XYZ=${ARCH}${MNH_INT}-${VERSION_XYZ}
 #                                                        #
 ##########################################################
 
+ifdef BULL
+  ifeq "$(BULL)" "YES"
+    LIBS       += -L/opt/softs/intel/compilers_and_libraries_2016.1.150/linux/compiler/lib/intel64 -L/opt/softs/intel/compilers_and_libraries_2016.1.150/linux/mkl/lib/intel64 -Wl,-rpath,/opt/softs/intel/compilers_and_libraries_2016.1.150/linux/compiler/lib/intel64 -Wl,-rpath,/opt/softs/intel/compilers_and_libraries_2016.1.150/linux/mkl/lib/intel64 -lintlc -lifport -limf -lifcore -lsvml -lirng
+  endif
+endif
+
+
+
 ##########################################################
 #           Source MYSRC                                 #
 ##########################################################
@@ -471,7 +479,13 @@ endif
 ifeq "$(VER_GRIBAPI)" "SOPRANO"
 GRIBAPI_PATH=/usr/local/sopra/grib_api
 INC_GRIBAPI   ?= -I${GRIBAPI_PATH}/include
-LIB_GRIBAPI   ?= -L${GRIBAPI_PATH}/lib -L${GRIBAPI_PATH}/lib64 -lgrib_api_f90 -lgrib_api
+LIB_GRIBAPI   ?= -L${GRIBAPI_PATH}/lib -L${GRIBAPI_PATH}/lib64 -lgrib_api_f90 -lgrib_api -Wl,-rpath,${GRIBAPI_PATH}/lib
+endif
+
+ifeq "$(VER_GRIBAPI)" "BULL"
+GRIBAPI_PATH=/opt/softs/libraries/ICC16.1.150/grib_api-1.15.0
+INC_GRIBAPI   ?= -I${GRIBAPI_PATH}/include
+LIB_GRIBAPI   ?= -L${GRIBAPI_PATH}/lib -L${GRIBAPI_PATH}/lib64 -lgrib_api_f90 -lgrib_api -Wl,-rpath,${GRIBAPI_PATH}/lib
 endif
 
 ifneq "x$(VER_GRIBAPI)" "x"
