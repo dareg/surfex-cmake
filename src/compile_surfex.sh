3ae403f602d7ac357747d1e31c97c9492b905dba
Patrick Samuelsson <patrick.samuelsson@smhi.se>
2018-03-01 19:08:11 +0100

    Added compile script

---
+++ src/compile_surfex.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+curdir=$PWD
+
+export ARCH=${ARCH=LXgfortran}
+
+export VER_MPI=${VER_MPI=NOMPI}
+
+#export OPTLEVEL=${OPTLEVEL=DEBUG}
+export OPTLEVEL=${OPTLEVEL=O2}
+
+export VER_USER=${VER_USER=""}
+#export VER_USER=${VER_USER="ASSIM_Sodankyla"}
+#export VER_USER=${VER_USER="MYSRC"}
+
+
+export XYZ=""
+source configure
+export SRC_SURFEX=""
+
+cd $curdir
+source ../conf/profile_surfex-${XYZ}
+
+cd $curdir
+
+if [[ $VER_USER = "" ]]; then
+  make
+  make installmaster
+else
+  make user
+  make installuser
+fi
+
+exit
