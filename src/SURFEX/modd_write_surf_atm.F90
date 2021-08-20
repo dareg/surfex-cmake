f6e022108a37525dbbf669e562c4bc5db88d3409
Patrick.Samuelsson <a000793@c21522.ad.smhi.se>
2021-08-20 15:37:25 +0200

    Ulf Andrae: Fix out of bounds problem in surfex output

--- src/SURFEX/modd_write_surf_atm.F90
+++ src/SURFEX/modd_write_surf_atm.F90
@@ -35,6 +35,8 @@
 IMPLICIT NONE
 !
 !-----------------------------------------------------------------------------------------------------
+! 
+INTEGER, PARAMETER :: MAX_NOWRITE_REC = 50000
 LOGICAL    :: LNOWRITE_CANOPY  ! flag used to avoid writing of canopy fields in OUTPUT file
 LOGICAL    :: LNOWRITE_TEXFILE ! flag used to avoid writing of tex file describing parameters
 !
@@ -46,7 +48,11 @@ LOGICAL :: LNAM_TEB_WRITTEN = .TRUE.
 LOGICAL :: LNAM_WATFLUX_WRITTEN = .TRUE.
 !
 LOGICAL :: LFIRST_WRITE = .TRUE.
-LOGICAL, DIMENSION(50000) :: LNOWRITE
+LOGICAL,           DIMENSION(MAX_NOWRITE_REC) :: LNOWRITE
+!$OMP THREADPRIVATE(LNOWRITE)
+CHARACTER(LEN=12), DIMENSION(MAX_NOWRITE_REC) :: CREC_TR
+!$OMP THREADPRIVATE(CREC_TR)
+
 INTEGER :: NCPT_WRITE = 0
 !
 LOGICAL :: LSPLIT_PATCH = .TRUE.
