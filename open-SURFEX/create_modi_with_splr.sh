4f2005e58d28b5ff7e8faa4e87cf283e1d6bbee7
Judicaël Grasset <judicael.grasset@meteo.fr>
2025-05-15 10:39:42 +0000

    add missing scripts

---
+++ open-SURFEX/create_modi_with_splr.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+set -x
+#FILEPATH=$1
+#FILENAME=$(basename "$FILEPATH")
+#
+#OUTFILE="modi_${FILENAME%.*}.intfb.h"
+#EXTENSION="${filename##*.}"
+#if [ ! $EXTENSION = ".F90" ]; then
+#	OUTFILE="modi_$FILENAME"
+#fi
+#FINAL_FILENAME="modi_$FILENAME"
+#
+#echo "Generate $OUTFILE from $FILENAME"
+#
+#./make_intfbl_f90.pl "$FILEPATH" 
+#if [ ! -f "$LOC_INTFBDIR/$OUTFILE" ]; then
+#	touch "$LOC_INTFBDIR/$OUTFILE"
+#fi
+#
+##echo "TO MOVE $LOC_INTFBDIR/modi_$FILENAME.intfb.h $LOC_INTFBDIR/modi_$FILENAME.F90"
+#mv "$LOC_INTFBDIR/$OUTFILE" "$LOC_INTFBDIR/$FINAL_FILENAME"
+
+FILEPATH=$1
+FILENAME=$(basename "$FILEPATH")
+OUTDIR=$2
+OUTFILE=""
+if [ $FILENAME == modi* ]; then
+	OUTFILE="spll_${FILENAME%.*}.f90"
+else
+	OUTFILE="spll_modi_${FILENAME%.*}.f90"
+fi
+
+./splr.pl --modi --odir="$OUTDIR" "$FILEPATH"
+if [ ! -f "$OUTDIR/$OUTFILE" ]; then
+	touch "$OUTDIR/$OUTFILE"
+fi
+
